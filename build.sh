1c1bdee16b0a1d3c03cd0117eb5e9ab06d8d63ed
dnminh1323 <minhdn@adigitrans.com>
2024-11-27 22:43:07 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
-# garble混淆安装: go install mvdan.cc/garble@latest
-GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 garble -literals -tiny build -ldflags "-w -s" -o build/cursor-vip_darwin_amd64;
-GOOS=darwin GOARCH=arm64 CGO_ENABLED=0 garble -literals -tiny build -ldflags "-w -s" -o build/cursor-vip_darwin_arm64;
+# garble 混淆安装: go install mvdan.cc/garble@latest
 # rsrc 应用程序图标安装: go install github.com/akavel/rsrc@latest
-rsrc -arch amd64 -ico rsrc.ico -o rsrc.syso;
-GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -ldflags "-s -w" -o build/cursor-vip_windows_amd64.exe;
-rm rsrc.syso;
-rsrc -arch arm -ico rsrc.ico -o rsrc.syso;
-GOOS=windows GOARCH=arm64 CGO_ENABLED=0 go build -ldflags "-w -s" -o build/cursor-vip_windows_arm64.exe;
-rm rsrc.syso;
 
-GOOS=linux GOARCH=amd64 CGO_ENABLED=0 garble -literals -tiny build -ldflags "-w -s" -o build/cursor-vip_linux_amd64;
-GOOS=linux GOARCH=arm64 CGO_ENABLED=0 garble -literals -tiny build -ldflags "-w -s" -o build/cursor-vip_linux_arm64;
+# Tạo thư mục build nếu chưa tồn tại
+mkdir -p build
 
-chmod +x build/install.sh;
-chmod +x build/cursor-vip_darwin_amd64;
-chmod +x build/cursor-vip_darwin_arm64;
-chmod +x build/cursor-vip_windows_amd64.exe;
-chmod +x build/cursor-vip_windows_arm64.exe;
-chmod +x build/cursor-vip_linux_amd64;
-chmod +x build/cursor-vip_linux_arm64;
+# Tạo file rsrc.syso với biểu tượng (icon)
+rsrc -arch amd64 -ico rsrc.ico -o rsrc.syso
+GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -ldflags "-s -w" -o build/cursor-vip_windows_amd64.exe
+rm rsrc.syso
+
+rsrc -arch arm -ico rsrc.ico -o rsrc.syso
+GOOS=windows GOARCH=arm64 CGO_ENABLED=0 go build -ldflags "-s -w" -o build/cursor-vip_windows_arm64.exe
+rm rsrc.syso
+
+# Phân quyền thực thi
+chmod +x build/cursor-vip_windows_amd64.exe
+chmod +x build/cursor-vip_windows_arm64.exe
