7bf83d695de3fb73f22885e1d8146756e29fb428
dnminh1323 <minhdn@adigitrans.com>
2024-11-28 01:32:37 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
-# garble 混淆安装: go install mvdan.cc/garble@latest
-# rsrc 应用程序图标安装: go install github.com/akavel/rsrc@latest
 
-# Tạo thư mục build nếu chưa tồn tại
-mkdir -p build
+# rsrc: go install github.com/akavel/rsrc@latest
+rsrc -arch amd64 -ico rsrc.ico -o rsrc.syso;
+GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -ldflags "-s -w" -o build/cursor-vip_windows_amd64.exe;
+rm rsrc.syso;
 
-# Tạo file rsrc.syso với biểu tượng (icon)
-rsrc -arch amd64 -ico rsrc.ico -o rsrc.syso
-GOOS=windows GOARCH=amd64 CGO_ENABLED=0 go build -ldflags "-s -w" -o build/cursor-vip_windows_amd64.exe
-rm rsrc.syso
 
-rsrc -arch arm -ico rsrc.ico -o rsrc.syso
-GOOS=windows GOARCH=arm64 CGO_ENABLED=0 go build -ldflags "-s -w" -o build/cursor-vip_windows_arm64.exe
-rm rsrc.syso
 
-# Phân quyền thực thi
-chmod +x build/cursor-vip_windows_amd64.exe
-chmod +x build/cursor-vip_windows_arm64.exe
+chmod +x build/install.sh;
+chmod +x build/cursor-vip_windows_amd64.exe;
